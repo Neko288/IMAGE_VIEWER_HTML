a154b4659928f3dac492d3eed438c3e053dfd752
Neko288 <79035426+Neko288@users.noreply.github.com>
2022-01-09 11:50:03 +0900

    START

---
+++ html(Viewer)/script.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+  const spinner = document.getElementById('loading');
+  spinner.classList.add('loaded');
+}
+
+function broke(){
+  const spinner2 = document.getElementById('loading');
+  spinner2.classList.add('loaded');
+}
+
