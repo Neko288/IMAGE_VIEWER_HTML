ac0e05a7eaaaf5d54d36ac42f6c69354ca17a474
Neko288 <ryzen.kuro@outlook.jp>
2022-03-22 00:47:56 +0900

    余白が縦と横で同じに大きさになるように調整、IE使用時にアラートする機能を追加
    見た目がちょっと良くなった？

--- html(Viewer)/script.js
+++ html(Viewer)/script.js
@@ -1,3 +1,10 @@
+window.onload = function () {
+  var userAgent = window.navigator.userAgent.toLowerCase();
+  if(userAgent.indexOf('msie') !== -1 || userAgent.indexOf('trident') !== -1){
+    alert('お使いのブラウザ(Internet Explorer)は動作対象外です。つまり、お使いのブラウザではこのサイトを正常に利用することはできません。FirefoxかChrome系(Chromium)のブラウザは動作対象確認済みですので、そちらの利用をおすすめします。')
+  };
+}
+
 window.onload = function() {
   const spinner = document.getElementById('loading');
   spinner.classList.add('loaded');
@@ -7,4 +14,3 @@ function broke(){
   const spinner2 = document.getElementById('loading');
   spinner2.classList.add('loaded');
 }
-
